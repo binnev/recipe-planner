9b884c9897e949430c9867d245aa0be9c939d915
Robin Neville <byntje@gmail.com>
2019-05-21 19:24:54 +0100

    Removed script.py. Why not just have the main body of the program in the same file as the functions?

--- script.py
+++
@@ -1,9 +0,0 @@
-import glob
-from recipe_functions import *
-
-recipe_files = glob.glob("recipes/*.txt")
-recipes = []
-for file in recipe_files:
-    recipes.append(load_recipe(file))
-INGREDIENTS, EQUIPMENT = make_shopping_list(recipes)
-print_shopping_list(INGREDIENTS)
