b0c93533abc998f8b6bf8d56f728991c7401c5b4
binnev <byntje@gmail.com>
2022-04-09 00:45:47 +0200

    Simple admin

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipe.models import Recipe, Ingredient, Equipment
+
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 0
+    fields = (
+        "amount", "unit", "description", "preparation"
+    )
+
+
+class EquipmentInline(admin.TabularInline):
+    model = Equipment
+    extra = 0
+    fields = ("name",)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    fields = (
+        "name",
+        "author",
+        "prep_time",
+        "cook_time",
+        "serves", "source", "image", "notes"
+    )
+    inlines = (
+        IngredientInline, EquipmentInline,
+    )
