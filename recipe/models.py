ec5982d2f7dbac41b811f0b8c660f785e58ea614
binnev <byntje@gmail.com>
2022-04-08 18:37:32 +0200

    Added models.

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, related_name="recipes", on_delete=models.CASCADE)
+    prep_time = models.IntegerField(null=True)
+    cook_time = models.IntegerField(null=True)
+    serves = models.CharField(null=True, blank=True, max_length=10)
+    source = models.CharField(null=True, blank=True, max_length=100)
+    image = models.CharField(null=True, blank=True, max_length=100)
+    notes = models.TextField()
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name="ingredients", on_delete=models.CASCADE)
+    amount = models.FloatField()
+    unit = models.CharField(max_length=15)
+    description = models.CharField(max_length=50)
+    preparation = models.CharField(max_length=50)
+
+
+class Equipment(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name="equipment", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
